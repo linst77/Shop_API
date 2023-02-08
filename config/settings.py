de59f875bb37016b4246f3c336276451c21ea0c6
linst77 <linst77@gmail.com>
2023-02-08 23:05:26 +0900

    update001

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-#ob0b#b2diimux&*r#tb5%8(ji9=p5o(^=7_z^ope%t144y@w^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', 'http://shop-api-front.s3-website.ap-northeast-2.amazonaws.com']
 
 
 # Application definition
